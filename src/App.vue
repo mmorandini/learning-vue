0139a4fbd4933510d1d05923f9c5bea41b340027
mmorandini <emmemorandini@gmail.com>
2018-02-26 15:50:47 +0000

    disable eslint, practise 2way-binding and event emitters

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <app-message></app-message>
   </div>
 </template>
 
---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+      <app-input :msg="message" @messageChanged="message = $event"></app-input>
+    </div>
+</template>
+
+<script>
+  import Input from './Input'
+
+  export default {
+    data () {
+      return {
+        message: 'Hello World!'
+      }
+    },
+    components: {
+      'app-input': Input
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
